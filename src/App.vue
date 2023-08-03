bfbb08c585f38b3222be3ccef84b27a492aabe96
绝恋。。 <1105033640@qq.com>
2023-08-03 08:36:19 +0800

    框架搭建，引入vant-ui

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
     <router-view/>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
+
+<script>
+
+export default {}
+</script>
+
+<style lang="less" scoped>
+
+</style>
 
 <style lang="less">
 #app {
