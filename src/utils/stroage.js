500447674929667ac4c74d4ed2a888e77db79114
绝恋。。 <1105033640@qq.com>
2023-08-04 14:33:05 +0800

    代码写错位置了，移动一下。。。

--- src/utils/stroage.js
+++ src/utils/stroage.js
@@ -18,3 +18,18 @@ export const getUserInfo = () => {
 export const delUserInfo = () => {
   localStorage.removeItem(USERINFO_KEY)
 }
+
+// 搜索相关存储方法
+
+const SEARCH_KEY = 'shop_search_history'
+
+// 获取搜索历史
+export const getSearchHistory = () => {
+  const history = localStorage.getItem(SEARCH_KEY)
+  return history ? JSON.parse(history) : []
+}
+
+// 设置搜索历史
+export const setSearchHistory = (history) => {
+  localStorage.setItem(SEARCH_KEY, JSON.stringify(history))
+}
