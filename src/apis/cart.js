a88984e3331d79bf97a8d06522a7d260285b60fe
绝恋。。 <1105033640@qq.com>
2023-08-06 14:54:54 +0800

    购物车相关api封装

---
+++ src/apis/cart.js
@@ -0,0 +1,33 @@
+// 存放购物车相关api
+
+import request from '@/utils/request'
+
+// 添加商品SKU到购物车中
+export const addSkuToCart = (goodsId, goodsNum, goodsSkuId) => {
+  return request.post('/cart/add', {
+    goodsId,
+    goodsNum,
+    goodsSkuId
+  })
+}
+
+// 获取购物车商品列表
+export const getCartList = () => {
+  return request.get('/cart/list')
+}
+
+// 购物车商品更新
+export const updateCartItem = (goodsId, goodsNum, goodsSkuId) => {
+  return request.post('/cart/update', {
+    goodsId,
+    goodsNum,
+    goodsSkuId
+  })
+}
+
+// 购物车商品更新
+export const deleteCartItem = (cartIds) => {
+  return request.post('/cart/clear', {
+    cartIds
+  })
+}
