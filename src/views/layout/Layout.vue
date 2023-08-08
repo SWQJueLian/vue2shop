ac89f77d48ca111885aa9da93cec35f71c79f8be
绝恋。。 <1105033640@qq.com>
2023-08-08 10:45:52 +0800

    修复：底部导航条高亮激活状态没有随着二级路由变化而变化的问题

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -2,11 +2,11 @@
   <div style="text-align: start;">
     <!--二级路由入口-->
     <router-view/>
-    <van-tabbar placeholder v-model="active" active-color="#ee0a24" inactive-color="#000">
+    <van-tabbar route placeholder v-model="active" active-color="#ee0a24" inactive-color="#000">
       <van-tabbar-item to="/home" name="home" icon="home-o">首页</van-tabbar-item>
-      <van-tabbar-item to="/category" name="search" icon="apps-o">分类</van-tabbar-item>
-      <van-tabbar-item to="/cart" name="friends" icon="shopping-cart-o" :badge="cartLength">购物车</van-tabbar-item>
-      <van-tabbar-item to="/user" name="setting" icon="user-o">我的</van-tabbar-item>
+      <van-tabbar-item to="/category" name="category" icon="apps-o">分类</van-tabbar-item>
+      <van-tabbar-item to="/cart" name="cart" icon="shopping-cart-o" :badge="cartLength">购物车</van-tabbar-item>
+      <van-tabbar-item to="/user" name="user" icon="user-o">我的</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
