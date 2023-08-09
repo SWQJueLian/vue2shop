8cd225e81878544092141438c9cf6fefc8cc638e
绝恋。。 <1105033640@qq.com>
2023-08-09 16:43:46 +0800

    用户中心动态渲染订单数量badge-2

--- src/apis/users.js
+++ src/apis/users.js
@@ -4,3 +4,9 @@ import request from '@/utils/request'
 export const getUserInfo = () => {
   return request.get('/user/info')
 }
+
+// 获取用户的商品状态数量
+// 玛德，这接口返回的数量不对的...有毒啊、、、草！
+export const getOrderTodoCount = () => {
+  return request.get('/order/todoCounts')
+}
