bb57a26960ac23c87824d686a3eb919eb9d7ff65
绝恋。。 <1105033640@qq.com>
2023-08-09 11:39:18 +0800

    订单页面tab标签实现过滤不同的订单类型

--- src/views/order/MyOrder.vue
+++ src/views/order/MyOrder.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="order">
     <van-nav-bar title="我的订单" left-arrow @click-left="$router.go(-1)" />
-    <van-tabs v-model="active">
+    <van-tabs v-model="active" @change="switchTab">
       <van-tab name="all" title="全部"></van-tab>
       <van-tab name="payment" title="待支付"></van-tab>
       <van-tab name="delivery" title="待发货"></van-tab>
@@ -28,10 +28,20 @@ export default {
     }
   },
   async created () {
-    // 订单类型，all-全部，payment-待支付，delivery-待发货，received-待收货，comment-待评价
-    const resp = await getOrderList(this.active, 1)
-    // console.log(resp, '订单列表')
-    this.orderDataList = resp.data.list.data
+    // 创建vue实例后发送请求初始化数据
+    await this.initData()
+  },
+  methods: {
+    async initData () {
+      // 订单类型，all-全部，payment-待支付，delivery-待发货，received-待收货，comment-待评价
+      const resp = await getOrderList(this.active, 1)
+      // console.log(resp, '订单列表')
+      this.orderDataList = resp.data.list.data
+    },
+    // 切换标签页时根据type类型重新加载数据
+    switchTab (name, title) {
+      this.initData()
+    }
   }
 }
 </script>
